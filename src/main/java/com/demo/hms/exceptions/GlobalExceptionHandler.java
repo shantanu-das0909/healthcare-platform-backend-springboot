c6636207e7ca9b9731d65d0ab34478678ae0a81c
Shantanu Das <71979060+shantanu-das0909@users.noreply.github.com>
2025-11-25 12:35:00 +0530

    remove auth from this app (#1)

--- src/main/java/com/demo/hms/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/demo/hms/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,8 @@
 package com.demo.hms.exceptions;
 
 import com.demo.hms.dto.ErrorResponse;
-import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -14,20 +12,12 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = {
             ResourceNotFoundException.class,
-            BadCredentialsException.class,
-            ExpiredJwtException.class,
             RuntimeException.class
     })
     public ResponseEntity<ErrorResponse> handleException(Exception ex, WebRequest request) {
         HttpStatus statusCode = HttpStatus.BAD_REQUEST;
         if(ex instanceof ResourceNotFoundException) {
             statusCode = HttpStatus.NOT_FOUND;
-        }
-        if(ex instanceof BadCredentialsException) {
-            statusCode = HttpStatus.UNAUTHORIZED;
-        }
-        if(ex instanceof  ExpiredJwtException) {
-            statusCode = HttpStatus.UNAUTHORIZED;
         }
         return new ResponseEntity<>(ErrorResponse.builder().message(ex.getMessage()).build(), statusCode);
     }
